04296372f751220b125a98af5b804676cd142d12
vumaasha <vumaasha@shakthi-2.0>
2016-08-15 18:13:17 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,49 @@
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.txt')) as f:
+    README = f.read()
+with open(os.path.join(here, 'CHANGES.txt')) as f:
+    CHANGES = f.read()
+
+requires = [
+    'pyramid',
+    'pyramid_chameleon',
+    'pyramid_jinja2',
+    'pyramid_debugtoolbar',
+    'pyramid_tm',
+    'SQLAlchemy',
+    'transaction',
+    'zope.sqlalchemy',
+    'waitress',
+    'authomatic'
+    ]
+
+setup(name='kiss',
+      version='0.0',
+      description='kiss',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        ],
+      author='',
+      author_email='',
+      url='',
+      keywords='web wsgi bfg pylons pyramid',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      test_suite='kiss',
+      install_requires=requires,
+      entry_points="""\
+      [paste.app_factory]
+      main = kiss:main
+      [console_scripts]
+      initialize_kiss_db = kiss.scripts.initializedb:main
+      """,
+      )
