c74844beac82fd1ac9906c03a41cf586f2d97ba6
Venkatesh <venkatesh@indix.com>
2016-08-17 16:01:07 +0530

    added jinja2 views

--- kiss/views/views.py
+++ kiss/views/views.py
@@ -7,15 +7,6 @@ from kiss.models import DBSession
 from kiss.models.models import MyModel
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
-def my_view(request):
-    try:
-        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
-    except DBAPIError:
-        return Response(conn_err_msg, content_type='text/plain', status_int=500)
-    return {'one': one, 'project': 'kiss'}
-
-
 conn_err_msg = """\
 Pyramid is having a problem using your SQL database.  The problem
 might be caused by one of the following things:
@@ -31,4 +22,18 @@ might be caused by one of the following things:
 After you fix the problem, please restart the Pyramid application to
 try it again.
 """
+
+@view_config(route_name='home', renderer='templates/base.html.jinja2')
+def my_view(request):
+    try:
+        one = DBSession.query(MyModel).filter(MyModel.name == 'one').first()
+    except DBAPIError:
+        return Response(conn_err_msg, content_type='text/plain', status_int=500)
+    return {'one': one, 'project': 'kiss'}
+
+@view_config(route_name='verify', renderer='templates/verify.html.jinja2')
+def verify(request):
+	return {'one': 1, 'project': 'kiss'}
+
+
 
