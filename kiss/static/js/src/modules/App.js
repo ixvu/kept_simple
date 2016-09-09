649196d1e884e07950c4c186f0e3a1ee150d27d4
Venkatesh <venkatesh@indix.com>
2016-09-09 14:01:24 +0530

    markAsMisclassification done

--- kiss/static/js/src/modules/App.js
+++ kiss/static/js/src/modules/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import 'script!jquery';
 require('skeleton-css/css/normalize.css')
 require('skeleton-css/css/skeleton.css')
 require('../../css/app.css')
@@ -25,11 +26,34 @@ export default React.createClass({
   	 let new_index = parseInt(event.target.attributes['data-new-index'].value)
   	 this.setState({'current_index': new_index})
   },
+  renderLevel(level, pos){
+		if (pos == 0){
+			return (<li className="top-level" data-cat-level={pos} onClick={this.markAsMisclassified} > {level} </li>)
+		} else {
+			return (<li data-cat-level={pos} onClick={this.markAsMisclassified}> {level}  </li>)
+		}
+  },
+  renderPath(path,name){
+  	let levels = this.getLevels(path);
+  	return (
+  		<div className="category-block" data-cat-name={name}>
+  			<ul>
+  				{ levels.map(this.renderLevel) }
+  			</ul>
+  		</div>
+  		);
+  },
+  markAsMisclassified(event){
+  	$(".category-block li").removeClass("error-level")
+  	$(event.target).addClass("error-level")
+  },
   render() {
   	let item = this.props.records[this.state.current_index]
   	let amazon_search_link = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords="+item.title
   	let prev_index = this.state.current_index == 0 ? 0 : this.state.current_index - 1;
   	let next_index = this.state.current_index == this.props.records.length -1 ? this.state.current_index : this.state.current_index + 1;
+  	let category_1 = item.category_1
+  	let category_2 = item.category_2
     return (
     <div className="container">
         <div className="row">
@@ -52,28 +76,10 @@ export default React.createClass({
 		        <img className="u-full-width" src="static/sample3.png"/>
         	</div>
         	<div className="two columns">
-	        	<div className="category-block">
-		        	<ul>
-		        		<li className="top-level"> Sports & Outdoors </li>
-		        		<li> Fan Shop </li>
-		        		<li> Clothing & Accessories </li>
-		        		<li> Sweatshirts </li>
-		        		<li className="error-level"> Kids </li>
-		        		<li> Boys </li>
-		        	</ul>
-	        	</div>
+        		{ this.renderPath(category_1,"category-1")}
         	</div>
         	<div className="two columns">
-	        	<div className="category-block">
-		        	<ul>
-		        		<li className="top-level"> Sports & Outdoors </li>
-		        		<li> Fan Shop </li>
-		        		<li> Clothing & Accessories </li>
-		        		<li className="error-level"> Sweatshirts </li>
-		        		<li> Kids </li>
-		        		<li> Boys </li>
-		        	</ul>
-		        </div>
+        		{ this.renderPath(category_2,"category-2")}
         	</div>
         </div>
     </div>
