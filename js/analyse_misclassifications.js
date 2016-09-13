5b6612bc9c3557483ca95cf18d1bf38bb229d147
Venkatesh <venkatesh@indix.com>
2016-09-13 18:59:56 +0530

    crawler working

--- js/analyse_misclassifications.js
+++
@@ -1,8 +0,0 @@
-var fs = require('fs');
-var parse = require('csv-parse');
-
-var parser = parse({delimiter: ','}, function(err, data){
-  console.log(data);
-});
-
-fs.createReadStream('/home/vumaasha/clothing_sub_category/spotcheck/round_2_both_wrong.csv').pipe(parser);
