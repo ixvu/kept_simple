dd1817262269729977f5578ba9102b077da6be31
Venkatesh <venkatesh@indix.com>
2016-09-13 18:58:36 +0530

    crawler working

---
+++ scraper/index.js
@@ -0,0 +1,27 @@
+var doc = " \
+Usage: \
+  index.js  --input=<input-json> \
+"
+var docopt = require('docopt');
+var Scraper = require('./scraper');
+var vo = require('vo');
+
+var options = docopt.docopt(doc, {version: '0.1.1rc'});
+console.log(options);
+
+scraper = new Scraper();
+var classificationRecords = require("./"+options["--input"])
+
+var run = function * () {
+  for (var i = 0; i < classificationRecords.length; i++) {
+  	let url = classificationRecords[i].url;
+  	let image = classificationRecords[i].id+".png";
+    yield scraper.scrape(url,image);
+  }
+  yield scraper.close();
+
+}
+
+vo(run)
+.then(out => console.log('out', out))
+.catch(e => console.error('error',error));
---
+++ scraper/scraper.js
@@ -0,0 +1,39 @@
+var Nightmare = require('nightmare');
+
+
+class Scraper {
+	constructor(){
+		this.scrapingOptions = {
+			x : 0,
+			y : 0,
+			width : 1024,
+			height: 768
+		}
+		this.nightmare = Nightmare({ 
+			"show" : true, 
+			"gotoTimeout" : 20000,
+			"ignore-certificate-errors" : true })
+		.viewport(this.scrapingOptions.width,this.scrapingOptions.height)
+		.useragent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0");
+	}	
+
+	scrape(url,path){
+		console.log(url)
+		var screenShot = this.nightmare.goto(url)
+		.then((result) => {
+			console.log('Http-Staus',result.code);
+			this.nightmare
+			.wait('body')
+			.screenshot(path,this.scrapingOptions);
+		})
+		.catch(error => console.log('error',error) );
+		console.log("done")
+		return screenShot;
+	}
+
+	close(){
+		return this.nightmare.end();
+	}
+}
+
+module.exports = Scraper;
