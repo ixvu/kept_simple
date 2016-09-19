55ed9a99cce9be0212854c6e771894bcd9cf977e
Venkatesh <venkatesh@indix.com>
2016-09-19 12:20:50 +0530

    * increased size of screen shot

--- scraper/scraper.js
+++ scraper/scraper.js
@@ -6,12 +6,12 @@ class Scraper {
 		this.scrapingOptions = {
 			x : 0,
 			y : 0,
-			width : 1024,
-			height: 768
+			width: 1024,
+			height: 2000
 		}
-		this.nightmare = Nightmare({ 
-			"show" : false, 
-			"gotoTimeout" : 20000,
+		this.nightmare = Nightmare({
+			"show" : false,
+			"gotoTimeout" : 30000,
 			"ignore-certificate-errors" : true })
 		.viewport(this.scrapingOptions.width,this.scrapingOptions.height)
 		.useragent("Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:47.0) Gecko/20100101 Firefox/47.0");
@@ -25,7 +25,7 @@ class Scraper {
 			statuses[id] = result.code;
 			this.nightmare
 			.wait('body')
-			.screenshot(path,this.scrapingOptions);
+			.screenshot(path);
 		})
 		.catch(error => console.log('error',error) );
 		console.log("done")
